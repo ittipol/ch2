25949d5bd0220581ff16dc7e367c9ee855c1130e
M <mail@mail.com>
2017-05-13 14:04:50 +0700

    fixed Layout

--- resources/views/pages/product/shelf.blade.php
+++ resources/views/pages/product/shelf.blade.php
@@ -41,7 +41,7 @@
           <div class="row">
 
             @foreach($shelf['products']['items'] as $product)
-              <div class="col-lg-4 col-xs-6">
+              <div class="col-md-4 col-xs-6">
 
                 <div class="card">
 
